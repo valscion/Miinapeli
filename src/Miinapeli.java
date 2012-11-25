19979a48e5145d6fb8b88e1b89a19a412c65a427
Vesa Laakso <laakso.vesa@gmail.com>
2012-11-25 21:43:08 +0200

    Hifistellyt valikkokuuntelijat ja oletus-exit-operaatio

--- src/Miinapeli.java
+++ src/Miinapeli.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -18,9 +20,6 @@ public class Miinapeli extends JFrame {
 	/** Alapaneelin sisältämä pelitilanteesta kertova tekstilabel */
 	private JLabel labelPelitila;
 
-	/** Ylävalikko */
-	private JMenuBar valikko;
-
 	public Miinapeli() {
 		// Ikkunan sisällön asettelija
 		this.setLayout(new BorderLayout());
@@ -39,18 +38,64 @@ public class Miinapeli extends JFrame {
 		this.add(this.paneeliAla, BorderLayout.SOUTH);
 
 		// Luodaan valikko
-		this.valikko = new JMenuBar();
-		this.valikko.setOpaque(true);
-
-		JMenu peliValikko = new JMenu("Peli");
-		peliValikko.add(new JMenuItem("Aloita alusta"));
-		peliValikko.addSeparator();
-		peliValikko.add(new JMenuItem("Lopeta"));
-		this.valikko.add(peliValikko);
-		this.add(this.valikko, BorderLayout.NORTH);
+		JMenuBar valikkopalkki = new JMenuBar();
+		JMenu pelivalikko = this.luoPelivalikko(valikkopalkki);
+		valikkopalkki.add(pelivalikko);
+		this.setJMenuBar(valikkopalkki);
 
 		// Asetetaan ikkunalle otsikko
 		this.setTitle("Miinaharava");
+	}
+
+	private JMenu luoPelivalikko(JMenuBar valikkopalkki) {
+		JMenu pelivalikko = new JMenu("Peli");
+
+		// Tapahtumien kuuntelija
+		ValikkoKuuntelija kuuntelija = new ValikkoKuuntelija();
+
+		JMenuItem aloitaAlusta = new JMenuItem("Aloita alusta");
+		aloitaAlusta.addActionListener(kuuntelija);
+		pelivalikko.add(aloitaAlusta);
+
+		pelivalikko.addSeparator();
+
+		JMenuItem lopeta = new JMenuItem("Lopeta");
+		lopeta.addActionListener(kuuntelija);
+		pelivalikko.add(lopeta);
+
+		return pelivalikko;
+	}
+
+	/** Valikon toiminnoista vastaava kuuntelija-sisäluokka. */
+	private class ValikkoKuuntelija implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent tapahtuma) {
+			String kasky = tapahtuma.getActionCommand();
+			System.out.println("Käsky kävi: " + kasky);
+
+			if (kasky.equals("Aloita alusta")) {
+				// Käskettiin aloittamaan peli alusta. Ei onnistu vielä.
+				final String alkupTxt = labelPelitila.getText();
+				final String kieltoTxt = "Vain rekisteröidyssä versiossa.";
+				Miinapeli.this.labelPelitila.setText(kieltoTxt);
+				// 2 sek päästä takaisin alkup. teksti.
+				if (!alkupTxt.equals(kieltoTxt)) {
+					Timer ajastin = new Timer(2000, new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							Miinapeli.this.labelPelitila.setText(alkupTxt);
+						}
+					});
+					ajastin.setRepeats(false);
+					ajastin.start();
+				}
+			}
+			else if (kasky.equals("Lopeta")) {
+				System.out.println("Peli sammuu.");
+				System.exit(0);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
@@ -62,6 +107,9 @@ public class Miinapeli extends JFrame {
 
 		// Asetetaan peli keskelle ruutua
 		peli.setLocationRelativeTo(null);
+
+		// Sammutetaan peli oletuksena kun ruksista klikataan
+		peli.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Pistetään peli näkymään.
 		peli.setVisible(true);
