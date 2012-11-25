459c56d9c1889d168a3e0d0968e1cba94a52b2cd
Vesa Laakso <laakso.vesa@gmail.com>
2012-11-25 20:56:23 +0200

    Siistimistä ja nimen muutos MiinaPeli -> Miinapeli

--- src/Miinapeli.java
+++ src/Miinapeli.java
@@ -2,61 +2,68 @@ import java.awt.*;
 
 import javax.swing.*;
 
-public class MiinaPeli extends JFrame {
+public class Miinapeli extends JFrame {
 
 	/**
 	 * Pakollinen serialVersionUID. Tai Eclipse valittaisi.
 	 */
 	private static final long serialVersionUID = 5734077433440873477L;
-	
+
 	/** Pelin sisältämä iso harmaa paneeli keskellä ruutua */
 	private JPanel paneeliKeski;
-	
+
 	/** Alapaneeli, jonka kautta tiedotetaan pelin tilasta. */
 	private JPanel paneeliAla;
-	
+
 	/** Alapaneelin sisältämä pelitilanteesta kertova tekstilabel */
 	private JLabel labelPelitila;
-	
+
 	/** Ylävalikko */
 	private JMenuBar valikko;
-	
-	public MiinaPeli() {
+
+	public Miinapeli() {
 		// Ikkunan sisällön asettelija
 		this.setLayout(new BorderLayout());
-		
+
 		// Keskipaneeli
 		this.paneeliKeski = new JPanel();
 		this.paneeliKeski.setPreferredSize(new Dimension(400, 300));
 		this.paneeliKeski.setBackground(Color.getHSBColor(0f, 0.0f, 0.5f));
 		this.add(this.paneeliKeski, BorderLayout.CENTER);
-		
+
 		// Alapaneeli
 		this.paneeliAla = new JPanel();
 		this.paneeliAla.setBackground(Color.getHSBColor(0.2f, 0.5f, 0.75f));
 		this.labelPelitila = new JLabel("Peli kesken", JLabel.CENTER);
 		this.paneeliAla.add(this.labelPelitila);
 		this.add(this.paneeliAla, BorderLayout.SOUTH);
-		
+
 		// Luodaan valikko
 		this.valikko = new JMenuBar();
 		this.valikko.setOpaque(true);
-		
+
 		JMenu peliValikko = new JMenu("Peli");
 		peliValikko.add(new JMenuItem("Aloita alusta"));
 		peliValikko.addSeparator();
 		peliValikko.add(new JMenuItem("Lopeta"));
 		this.valikko.add(peliValikko);
 		this.add(this.valikko, BorderLayout.NORTH);
-		
+
 		// Asetetaan ikkunalle otsikko
-		this.setTitle("Miinapeli");
+		this.setTitle("Miinaharava");
 	}
 
 	public static void main(String[] args) {
-		MiinaPeli peli = new MiinaPeli();
-		
+		Miinapeli peli = new Miinapeli();
+
+		// Asetetaan ikkunan kooksi se, jonka komponenttien haluamat koot
+		// määrittävät.
 		peli.pack();
+
+		// Asetetaan peli keskelle ruutua
+		peli.setLocationRelativeTo(null);
+
+		// Pistetään peli näkymään.
 		peli.setVisible(true);
 	}
 
